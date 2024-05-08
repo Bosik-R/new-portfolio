a95907490c8bdb3de22c3edc41c07cc0ca38e1ad
Bosik-R <62885510+Bosik-R@users.noreply.github.com>
2024-05-08 14:06:28 +0200

    Modify loading spinner

--- components/SplineScene/SplineScene.tsx
+++ components/SplineScene/SplineScene.tsx
@@ -22,13 +22,13 @@ const SplineScene = () => {
             layout
             initial={{ clipPath: 'circle(80% at 50% 50%)' }}
             exit={{
-              clipPath: 'circle(0% at 55% 43%)',
+              clipPath: 'circle(0% at 50% 50%)',
               transition: { duration: 1.5 },
             }}
             className='absolute flex w-full h-full justify-center items-center text-grayLight bg-mainBg'>
             <motion.div
               initial={{ x: 0, y: 0 }}
-              exit={{ x: 80, y: -60, scale: 0.3 }}
+              exit={{ x: 0, y: 0, scale: 0.3 }}
               transition={{ duration: 0.5, delay: 0.7 }}>
               <Spinner />
             </motion.div>
