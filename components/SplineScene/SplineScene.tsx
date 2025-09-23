f68e840c3f92b7cd5fba23b3982ad8def4254e94
Bosik-R <62885510+Bosik-R@users.noreply.github.com>
2025-09-23 12:45:53 +0200

    Spline sceen in public folder

--- components/SplineScene/SplineScene.tsx
+++ components/SplineScene/SplineScene.tsx
@@ -8,7 +8,7 @@ import Legend from '../Legend/Legend';
 import { usePathname } from 'next/navigation';
 import { twMerge } from 'tailwind-merge';
 
-const scene = 'https://prod.spline.design/geLURPu4mmgL0UrD/scene.splinecode';
+const scene = '/scene.splinecode';
 // const scene = process.env.NEXT_PUBLIC_SPLINE_SCENE ?? '';
 
 const SplineScene = () => {
